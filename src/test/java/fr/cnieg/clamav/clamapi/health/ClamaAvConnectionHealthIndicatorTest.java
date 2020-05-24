fecfa63d2bdd899b1da1d3ec4ac0e1433ee2bcc7
Guillaume Audic <audic.gui@gmail.com>
2020-05-24 14:14:14 +0200

    build(test): improve unit test health probe
    Signed-off-by: Guillaume Audic <audic.gui@gmail.com>

--- src/test/java/fr/cnieg/clamav/clamapi/health/ClamaAvConnectionHealthIndicatorTest.java
+++ src/test/java/fr/cnieg/clamav/clamapi/health/ClamaAvConnectionHealthIndicatorTest.java
@@ -9,11 +9,17 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.actuate.endpoint.SecurityContext;
+import org.springframework.boot.actuate.endpoint.http.ApiVersion;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.health.CompositeHealth;
+import org.springframework.boot.actuate.health.HealthComponent;
+import org.springframework.boot.actuate.health.HealthEndpointWebExtension;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import fr.cnieg.clamav.clamapi.services.ClamAVService;
@@ -24,27 +30,38 @@ import fr.cnieg.clamav.clamapi.services.ClamAVService;
 public class ClamaAvConnectionHealthIndicatorTest {
 
     @Autowired
-    ApplicationAvailability availability;
-
-    @Autowired
     ClamaAvConnectionHealthIndicator clamaAvConnectionHealthIndicator;
 
     @MockBean
     ClamAVService clamAVService;
 
+    @Autowired
+    HealthEndpointWebExtension healthEndpointWebExtension;
+
+    private final String CLAMAV_CONNECTION_PROBE = "clamaAvConnection";
+
     @Test
     public void givenStatusOk_whenPing_thenReadinessProbeOk() throws Exception {
 
         doNothing().when(clamAVService).ping();
-        Health health = clamaAvConnectionHealthIndicator.health();
-        assertEquals("UP", health.getStatus().toString());
+        clamaAvConnectionHealthIndicator.health();
+        WebEndpointResponse<HealthComponent> response = healthEndpointWebExtension.health(ApiVersion.V3, SecurityContext.NONE, true, "readiness");
+
+        CompositeHealth compositeHealth = (CompositeHealth) response.getBody();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(Status.UP, compositeHealth.getComponents().get(CLAMAV_CONNECTION_PROBE).getStatus());
     }
 
     @Test
     public void givenStatusKo_whenPing_thenReadinessProbeKo() throws Exception {
 
         doThrow(new IOException("Can not ping clamav")).when(clamAVService).ping();
-        Health health = clamaAvConnectionHealthIndicator.health();
-        assertEquals("DOWN", health.getStatus().toString());
+        clamaAvConnectionHealthIndicator.health();
+
+        WebEndpointResponse<HealthComponent> response = healthEndpointWebExtension.health(ApiVersion.V3, SecurityContext.NONE, true, "readiness");
+
+        CompositeHealth compositeHealth = (CompositeHealth) response.getBody();
+        assertEquals(HttpStatus.SERVICE_UNAVAILABLE.value(), response.getStatus());
+        assertEquals(Status.DOWN, compositeHealth.getComponents().get(CLAMAV_CONNECTION_PROBE).getStatus());
     }
 }
